ca8c5d0b05e6c068f8c7cefe52780fcee088301b
JisooJang <ppuagirls@naver.com>
2019-05-15 17:36:22 +0900

    commit until 12th lectures (Functional = dump Component)

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
 
 // 컴포넌트2. 각 영화 리스트 박스 컴포넌트
+/*
 class Movie extends Component {
 
     static propTypes = {
@@ -19,9 +20,20 @@ class Movie extends Component {
         );
     }
 }
+*/
+
+function Movie({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    );
+}
 
 
 // 컴포넌트3. 각 영화 포스터 이미지 컴포넌트.
+/*
 class MoviePoster extends Component {
 
     static propTypes = {
@@ -30,9 +42,26 @@ class MoviePoster extends Component {
 
     render() {
         return (
-            <img src={this.props.poster}/>
+            <img src={this.props.poster} alt="Movie Poster"/>
         );
     }
+}
+*/
+
+
+function MoviePoster({poster}) {
+    return (
+        <img src={poster} alt="Movie Poster"/>
+    );
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
